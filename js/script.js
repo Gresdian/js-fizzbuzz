ccb3481913bb9cc6dbebbeea0f429f0e07649caf
Gresdian <chris_da98@outlook.it>
2023-11-23 09:57:15 +0100

    exercise correct

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 const first_list = document.getElementById('first-list');
 
 for(let i=1; i<=100; i++){
-    let Fizz , Buzz , FizzBuzz;
-    let li = `<li id="item-${i}">${i}</li>`;
-    first_list.innerHTML += li;
-    
-    if(i%15 == 0){
-      FizzBuzz = li ;
-      li.setAttribute('id',`list-item-${i}`);
-      FizzBuzz.innerHTML = `FizzBuzz ${i}`;
-    }
+  let value;
+  
+  if(i%3 == 0  && i%5 == 0){
+    value = 'FizzBuzz';
+    console.log('FizzBuzz');
+  }
+  else if(i%5 == 0){
+    value = 'Buzz';
+    console.log('Buzz');
+  }
+  else if(i%3 == 0){
+    value = 'Fizz';
+    console.log('Fizz');
+  }
+  else{
+    value = i;
+    console.log(i);
+  }
 }
+
+let li = document.createElement('li');
+li.innerText = value;
+first_list.appendChild(li);
